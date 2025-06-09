dce1b1ec520b6d42e103b35423934479932bf31f
BadRothenfelde <dizzyrascalger@gmail.com>
2025-06-09 22:28:32 +0200

    weitere Frontend Aufräumarbeiten

--- frontend/src/app/play/play.component.ts
+++ frontend/src/app/play/play.component.ts
@@ -10,47 +10,50 @@ import { CommonModule } from '@angular/common';
   styleUrl: './play.component.css'
 })
 export class PlayComponent implements OnInit, OnDestroy {
-  // suits = ['♠', '♥', '♦', '♣']
-  // Spades, Heart, Diamond, Clubs
   private sub!: Subscription;
-  public phase: String = "lobby";
-  public players: {[key: string]:{
-    "username": string,
-    "cards": []
-    "state": string,
-    "score": number
-  } } = {};
-  public cards:any[] = [];
+  public phase: string = "lobby";
+  public players: {
+    [key: string]: {
+      username: string,
+      cards: [],
+      state: string,
+      score: number
+    }
+  } = {};
+  public cards: any[] = [];
   public player: string = "";
   public room = "";
   public master = "";
   public turn_player = "";
   public card: string = "";
   public correct: boolean = false;
-  public score: number= 0;
-  public pyramid:[] = [];
-  public openPyramid:boolean[] = Array(10).fill(false);
+  public score: number = 0;
+  public pyramid: [] = [];
+  public openPyramid: boolean[] = Array(10).fill(false);
 
-  public notification : "message" | null = null;
-  public message : {title: string, message: string} = {title: "None", message: "error"}
+  public notification: "message" | null = null;
+  public message: { title: string, message: string } = { title: "None", message: "error" };
+
   constructor(private wsService: WebSocketService) {}
 
   ngOnInit(): void {
     const room = new URLSearchParams(window.location.search).get('room');
     const user = new URLSearchParams(window.location.search).get('username');
-    if (room != null || room == ""){
+    if (room != null || room == "") {
       if (user != null || user == "")
         this.wsService.connect(room, user);
       else
         this.wsService.connect(room, "");
+    } else {
+      window.location.href = "/";
     }
-      
-    else
-      window.location.href="/"
+
+    // ESC zum Schließen der Notification
+    window.addEventListener('keydown', this.handleEscKey);
 
     this.wsService.connectionStatus$.subscribe((status) => {
       if (status === 'error') {
-       // window.location.href="/"
+        // window.location.href="/"
       }
     });
 
@@ -58,119 +61,135 @@ export class PlayComponent implements OnInit, OnDestroy {
       console.log('[Frontend] Message received:', msg);
 
       const type = msg.type;
-      
-      if(type == 'state'){
+
+      if (type == 'state') {
         this.phase = msg.phase;
         this.players = msg.players;
         this.cards = this.players[this.player]['cards'];
         this.score = this.players[this.player]['score'];
         this.master = msg.master;
         this.room = msg.room;
-      } else if (type == "guess_result"){
+      } else if (type == "guess_result") {
         this.card = msg.card;
         this.correct = msg.correct;
-        
         this.turn_player = msg.next_player;
-        if(this.phase == "qualifying" && this.player == this.turn_player){
-          this.notification = "message"
-          this.message.title = "Benachrichtigung"
-          this.message.message = "Sie sind nun am Zug"
-          
-        } else if(this.phase == "bus" && this.player == this.turn_player){
-          this.notification = "message"
-          this.message.title = "Benachrichtigung"
-          if(this.correct)
-            this.message.message = "Sie lagen Richtig"
+        if (this.phase == "qualifying" && this.player == this.turn_player) {
+          this.notification = "message";
+          this.message.title = "Der Bus will weiterfahren!";
+          this.message.message = "Fahre fort oder der Bus fährt fort.";
+          setTimeout(() => {
+            this.notification = null;
+          }, 5000);
+        } else if (this.phase == "bus" && this.player == this.turn_player) {
+          this.notification = "message";
+          this.message.title = "Benachrichtigung";
+          if (this.correct)
+            this.message.message = "Du lagst richtig!";
           else
-            this.message.message = "Sie lagen Falsch"
-          
+            this.message.message = "Du lagst falsch!";
         }
-          
-      } else if(type == "pyramid"){
-        this.pyramid = msg.pyramid; 
-      } else if(type == "pyramidCard"){
+      } else if (type == "pyramid") {
+        this.pyramid = msg.pyramid;
+      } else if (type == "pyramidCard") {
         this.openPyramid[msg.pyramid_id] = true;
-        console.log(this.openPyramid)
-      } else if(type == "pyramid_reveal"){
-        this.notification = "message"
-        this.message.title = "Kartenoffenbarung"
-        this.message.message = "Wert ihrer Karte " + msg.message + " gleicht der Offenbarung!"
-      }else if(type == 'player'){
+        console.log(this.openPyramid);
+      } else if (type == "pyramid_reveal") {
+        this.notification = "message";
+        this.message.title = "Treffer!!!";
+        this.message.message = `Der Wert deiner Karte ${msg.message} gleicht der aufgedeckten Karte! Verteile einen Schluck an einen Mitspieler!`;
+      } else if (type == 'player') {
         this.player = msg.player;
       }
-      console.log(this.card)
-      // handle game state, results, etc.
+      console.log(this.card);
     });
-
   }
+
+
+  handleEscKey = (event: KeyboardEvent) => {
+    if (event.key === 'Escape' && this.notification) {
+      this.notification = null;
+    }
+  };
+
+  ngOnDestroy1(): void {
+    this.sub.unsubscribe();
+    this.wsService.close();
+    window.removeEventListener('keydown', this.handleEscKey);
+  }
+
   getPlayerNames(): string[] {
     return Object.keys(this.players);
   }
+
   getCardCount(): number {
     return this.cards.length;
   }
-  startGame() {
 
-     if(this.master == this.player){
+  startGame() {
+    if (this.master == this.player) {
       this.wsService.sendMessage({ action: 'start' });
-      this.turn_player = this.master
-     }
-    else{
-      this.notification = "message"
-      this.message.title = "Verbot"
-      this.message.message = "Erbitten Sie den Spielmeister die Spiele zu beginnen!"
+      this.turn_player = this.master;
+    } else {
+      this.notification = "message";
+      this.message.title = "Startklar?";
+      this.message.message = "Frag den Game Master, ob er bereit ist!";
     }
-    
   }
+
   sendGuess(guess: string) {
-    if(this.turn_player == this.player)
+    if (this.turn_player == this.player) {
       this.wsService.sendMessage({ action: 'guess', guess });
-    else{
-      this.notification = "message"
-      this.message.title = "Verbot"
-      this.message.message = "Sie sind nicht am Zug"
+    } else {
+      this.notification = "message";
+      this.message.title = "Hab Geduld!";
+      this.message.message = "Du bist nicht am Zug.";
     }
   }
+
   allOpened(): boolean {
     return this.openPyramid.slice(0, 9).every(value => value === true);
   }
+
   sendGuessBus(guess: string) {
     this.wsService.sendMessage({ action: 'guessBus', guess });
   }
-  advance(){
-    if(this.master == this.player){
+
+  advance() {
+    if (this.master == this.player) {
       this.wsService.sendMessage({ action: 'next_phase' });
-     }
-    else{
-      this.notification = "message"
-      this.message.title = "Verbot"
-      this.message.message = "Erbitten Sie den Spielmeister die Spiele in die nächste Phase zu leiten!"
+    } else {
+      this.notification = "message";
+      this.message.title = "Stop!";
+      this.message.message = "Bitte den Game Master die nächste Runde zu starten!";
     }
-    
   }
-  openCard(id:number){
-     if(this.master == this.player)
+
+  openCard(id: number) {
+    if (this.master == this.player) {
       this.wsService.sendMessage({ action: 'reveal_pyramid', id });
-    else{
-      this.notification = "message"
-      this.message.title = "Verbot"
-      this.message.message = "Erbitten Sie den Spielmeister die Karte zu offenbaren"
+    } else {
+      this.notification = "message";
+      this.message.title = "Stop!";
+      this.message.message = "Bitte den Game Master die nächste Karte aufzudecken!";
     }
   }
+
   sendChat(message: string) {
     this.wsService.sendMessage({ action: 'chat', message });
   }
-  done(){  
-    this.notification = "message"
-    this.message.title = "Dankaussagung"
-    this.message.message = "Vielen Dank fürs Spielen! Eine Spende für die Entwickler wäre schätzenswert!"
+
+  done() {
+    this.notification = "message";
+    this.message.title = "Finito!";
+    this.message.message = "Vielen Dank fürs Spielen! Wir wünschen weiterhin einen guten Durst!";
   }
+
   ngOnDestroy(): void {
     this.sub.unsubscribe();
     this.wsService.close();
   }
 
-  toggleModal(modal: 'message' | null){
+  toggleModal(modal: 'message' | null) {
     this.notification = modal;
   }
 }
